473d1fedef86beb51febfcff6a1aef1d82814b65
coding-mart <69900149+coding-mart@users.noreply.github.com>
2020-09-07 21:50:08 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b=0,c;
+    cout<<"enter the number for generating table ?";
+    cin>>a;
+    do
+    {
+        b++;
+        c=a*b;
+        cout<<a<<"*"<<b<<"="<<c<<endl;
+
+    }while(b<10);
+
+    return 0;
+}
